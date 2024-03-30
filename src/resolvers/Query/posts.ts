6221b72633f3b0243b9c18f2cdb04336362add31
BaHwan Han <beingbook@gmail.com>
2024-03-30 18:36:13 +0900

    welcome hell

--- src/resolvers/Query/posts.ts
+++ src/resolvers/Query/posts.ts
@@ -1,3 +1,9 @@
+import { eq } from "drizzle-orm";
+
+import { $posts } from "~/data/Post.schema";
+import { $users } from "~/data/User.schema";
+import { getDatabase } from "~/data/database";
+
 import type { QueryResolvers } from "./../__generated__/ResolverTypes";
 
 export const posts: NonNullable<QueryResolvers["posts"]> = async (
@@ -5,6 +11,28 @@ export const posts: NonNullable<QueryResolvers["posts"]> = async (
   _arg,
   _ctx,
 ) => {
-  /* Implement Query.posts resolver logic here */
-  return [];
+  const database = await getDatabase();
+  const result = await database
+    .select({
+      post: $posts,
+      author: $users,
+    })
+    .from($posts)
+    .innerJoin($users, eq($posts.authorId, $users.id));
+
+  return result.map(({ post, author }) => ({
+    __typename: "Post",
+    id: post.id,
+    title: post.title!,
+    body: post.body!,
+    createdAt: post.createdAt,
+    author: {
+      __typename: "User",
+      id: author.id,
+      name: author.name!,
+      comments: [],
+      posts: [],
+    },
+    comments: [],
+  }));
 };
--- src/resolvers/Query/user.ts
+++ src/resolvers/Query/user.ts
@@ -1,3 +1,10 @@
+import { eq } from "drizzle-orm";
+
+import { $comments } from "~/data/Comment.schema";
+import { $posts } from "~/data/Post.schema";
+import { $users } from "~/data/User.schema";
+import { getDatabase } from "~/data/database";
+
 import type { QueryResolvers } from "./../__generated__/ResolverTypes";
 
 export const user: NonNullable<QueryResolvers["user"]> = async (
@@ -5,6 +12,60 @@ export const user: NonNullable<QueryResolvers["user"]> = async (
   _arg,
   _ctx,
 ) => {
-  /* Implement Query.user resolver logic here */
-  return null;
+  const database = await getDatabase();
+  const userResult = await database
+    .select()
+    .from($users)
+    .where(eq($users.id, Number(_arg.id)));
+
+  if (userResult.length === 0) {
+    return null;
+  }
+  if (userResult.length > 1) {
+    throw new Error("More than one user found");
+  }
+  const [user] = userResult;
+  const postResult = await database
+    .select()
+    .from($posts)
+    .where(eq($posts.authorId, Number(user.id)));
+  const commentResult = await database
+    .select()
+    .from($comments)
+    .where(eq($comments.authorId, Number(user.id)));
+
+  return {
+    __typename: "User",
+    id: user.id,
+    name: user.name!,
+    createdAt: user.createdAt,
+    posts: postResult.map((post) => ({
+      __typename: "Post",
+      ...post,
+      title: post.title!,
+      body: post.body!,
+      author: {
+        __typename: "User",
+        ...user,
+        name: user.name!,
+        createdAt: user.createdAt,
+        comments: [],
+        posts: [],
+      },
+      comments: [],
+    })),
+    comments: commentResult.map((comment) => ({
+      __typename: "Comment",
+      ...comment,
+      body: comment.body!,
+      post: null as any,
+      author: {
+        __typename: "User",
+        ...user,
+        name: user.name!,
+        comments: [],
+        posts: [],
+      },
+    })),
+  };
 };
